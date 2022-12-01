d30f8ba7c721a23ffd1c21a2ffc3993019c80192
asus <casadadaista@gmail.com>
2022-12-01 07:33:34 -0300

    bw-1

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import "./dist/style.css";
+</script>
+
+<main>
+  <section>
+    <h1>hello</h1>
+  </section>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
