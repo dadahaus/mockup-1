d9869ffb8c00d99bbe2553fd1aa62a36bb246994
asus <casadadaista@gmail.com>
2022-12-08 13:50:45 -0300

    first section

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,6 +1,6 @@
 <section>
   <div class="one"><h1>welcome to the haus</h1></div>
-  <div class="two">
+  <div class="two column">
     <h3>design resources for the people</h3>
     <button>
       <a href="#">go inside</a>
@@ -13,11 +13,26 @@
     display: flex;
     flex-direction: row;
     justify-content: space-around;
+    &>*{
+      display: flex;
+      height:100vh;
+      
+    }
     .one{
       text-transform: capitalize;
-    }
-    .two{
+      justify-content: center;
+      align-items: center;
+      flex-basis: 70%;
 
     }
+    .two{
+      align-items: flex-end;
+      gap:2rem;
+      text-align: end;
+      flex-basis: 30%;
+
+      
+    }
+ 
   }
 </style>
