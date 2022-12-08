64e7b89218abc783fd9ef4106539852a8c167333
asus <casadadaista@gmail.com>
2022-12-08 13:37:16 -0300

    initial commit

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,6 +1,23 @@
 <section>
-  <h1>hello</h1>
+  <div class="one"><h1>welcome to the haus</h1></div>
+  <div class="two">
+    <h3>design resources for the people</h3>
+    <button>
+      <a href="#">go inside</a>
+    </button>
+  </div>
 </section>
 
 <style lang="scss">
+  section{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    .one{
+      text-transform: capitalize;
+    }
+    .two{
+
+    }
+  }
 </style>
