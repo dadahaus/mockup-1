5a46af95957e6d2cf285d6fcf6b1acb81ec4f877
asus <casadadaista@gmail.com>
2022-12-08 18:20:42 -0300

    third section finished

--- src/lib/Third.svelte
+++ src/lib/Third.svelte
@@ -1,14 +1,60 @@
 <section class={$$props.class}>
-  <h1>hello</h1>
+  <h3 class="one">chooooooose your ideation</h3>
+  <div class="two">
+    <a href="http://"><div></div></a>
+    
+  
+  </div>
+  <h5 class="three">design resources for the people</h5>
+  <h5 class="four">design resources for the people</h5>
+  <h5 class="five">design resources for the people</h5>
 </section>
 
 <style lang="scss">
   .section {
+    position: relative;
     &__3 {
-      background: center/ cover no-repeat #eee url("../../assets/bgr-1.gif");
-      h1{
-          font-size: 7rem;
+      overflow: hidden;
+      background: center/ cover no-repeat #eee url("../../assets/bgr-2.gif");
+      gap: 3rem;
+      .one{
+        max-width: 10ch;
+        text-align: center;
+        margin-bottom: -6rem;
+        z-index: 99;
       }
+      .two{
+        display: flex;
+        gap: 1rem;
+        &>*{
+          height: var(--big);
+          width: var(--small);
+          &:nth-child(1){background: center/ cover no-repeat #eee url("../../assets/1.png");}
+          
+        }
+      }
+    }
+    h5{
+        max-width: 15ch;
+
+    }
+    .three{
+       
+
+      position: absolute;
+      top: 50%;
+      left: 10%;
+      transform: translatey(-50%);
+    }
+      .four{
+
+      position: absolute;
+      bottom: 10%;
+      right: 10%;
+    }  .five{
+      position: absolute;
+      top: 10%;
+      right: 10%;
     }
   }
 </style>
